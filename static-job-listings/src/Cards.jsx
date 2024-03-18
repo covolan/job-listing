12f1146d0a7a7557261adac9863fe72e822e8694
covolan <covolan09@gmail.com>
2024-03-18 17:00:56 -0300

    Did most part of the logic

--- static-job-listings/src/Cards.jsx
+++ static-job-listings/src/Cards.jsx
@@ -3,6 +3,63 @@ import Data from "../data.json";
 import "./style.css";
 
 const Cards = () => {
+  const clickfunc = (e) => {
+    const filters = document.querySelector(".filters");
+    const eventButton = document.createElement("button");
+    const card = document.querySelectorAll(".card");
+    const filterDiv = document.querySelector(".filter__div");
+    const clearBtn = document.querySelector(".clear");
+
+    eventButton.innerText = e.currentTarget.innerText;
+    eventButton.classList.add("filter");
+    eventButton.id = e.currentTarget.innerText;
+    const filtersTracker = [];
+
+    let res = [];
+
+    Data.forEach((data) => {
+      if (
+        data[e.currentTarget.classList[1]].includes(e.currentTarget.innerText)
+      ) {
+        res.push(data.id);
+      }
+    });
+
+    card.forEach((c) => {
+      if (!res.includes(parseInt(c.id))) {
+        c.classList.add("hidden");
+      }
+    });
+
+    if (!filtersTracker.includes(eventButton.id)) {
+      filters.append(eventButton);
+      filterDiv.classList.remove("not__show");
+      filtersTracker.push(eventButton.id);
+      eventButton.addEventListener("click", (event) => {
+        let perRes = res.slice(0, res.length);
+        card.forEach((c) => {
+          if (!perRes.includes(parseInt(c.id))) {
+            c.classList.remove("hidden");
+          }
+        });
+        filtersTracker.splice(filtersTracker.indexOf(event.target.id), 1);
+        if (filtersTracker.length == 0) {
+          filterDiv.classList.add("not__show");
+        }
+        eventButton.remove();
+      });
+    }
+
+    clearBtn.addEventListener("click", (event) => {
+      card.forEach((c) => {
+        c.classList.remove("hidden");
+      });
+      filterDiv.classList.add("not__show");
+      filtersTracker.length = 0;
+      filters.innerHTML = "";
+    });
+  };
+
   return (
     <>
       {Data.map((job) => (
@@ -25,13 +82,27 @@ const Cards = () => {
             </div>
           </div>
           <div className="card__tech">
-            <button className="tech level">{job.level}</button>
+            <button onClick={(e) => clickfunc(e)} className="tech level">
+              {job.level}
+            </button>
             {job.languages.map((lang) => (
-              <button key={lang} className="tech">{lang}</button>
+              <button
+                onClick={(e) => clickfunc(e)}
+                key={lang}
+                className="tech languages"
+              >
+                {lang}
+              </button>
             ))}
             {job.tools
               ? job.tools.map((tool) => (
-                  <button key={tool} className="tech tools">{tool}</button>
+                  <button
+                    onClick={(e) => clickfunc(e)}
+                    key={tool}
+                    className="tech tools"
+                  >
+                    {tool}
+                  </button>
                 ))
               : null}
           </div>
